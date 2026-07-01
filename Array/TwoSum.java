ff4fb01b08c7f9d53cc2f0bef71155b3a55ad281
Sahilsanjaysingh <7814384979sahil@gmail.com>
2026-07-01 06:10:34 +0530

    Two sum

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -1,5 +1,30 @@
 package Array;
+import java.util.*;
 
 public class TwoSum {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[]= new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        int t= sc.nextInt();
+
+        Solution obj= new Solution();
+        boolean ans=obj.target(arr, t);
+        System.out.println(ans);
+    }
+    static class Solution{
+        public boolean target(int arr[], int t){
+            for(int i=0; i<arr.length; i++){
+                for(int j=i+1; j<arr.length; j++){
+                    if(arr[i]+arr[j]==t){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
 }
