38969e69489eab8e0f8fc4caf59ffecf4198edab
Sahilsanjaysingh <7814384979sahil@gmail.com>
2026-07-01 06:26:58 +0530

    Two sum using hashmap

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -17,13 +17,22 @@ public class TwoSum {
     }
     static class Solution{
         public boolean target(int arr[], int t){
+            // for(int i=0; i<arr.length; i++){
+            //     for(int j=i+1; j<arr.length; j++){
+            //         if(arr[i]+arr[j]==t){
+            //             return true;
+            //         }
+            //     }
+            // }
+
+            HashMap<Integer, Integer> see= new HashMap<>();
             for(int i=0; i<arr.length; i++){
-                for(int j=i+1; j<arr.length; j++){
-                    if(arr[i]+arr[j]==t){
-                        return true;
-                    }
+                int comp= t- arr[i];
+                if(see.containsKey(comp)){
+                    return true;
                 }
-            }
+                see.put(arr[i], i);
+            } 
             return false;
         }
     }
