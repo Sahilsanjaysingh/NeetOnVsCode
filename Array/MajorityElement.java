72ad9c661b7512c606709e39db39b7e5384ed42f
Sahilsanjaysingh <7814384979sahil@gmail.com>
2026-07-04 21:07:35 +0530

    MajorityElement

---
+++ Array/MajorityElement.java
@@ -0,0 +1,29 @@
+package Array;
+import java.util.*;
+public class MajorityElement {
+public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int n= sc.nextInt();
+    int arr[]= new int[n];
+    for(int i=0; i<n; i++){
+        arr[i]=sc.nextInt();
+    }
+      Solution obj = new Solution();
+        int ans =obj.Majority(arr);
+        System.out.print(ans);
+   }
+   static class  Solution{
+    public int Majority(int ans[]){
+        HashMap<Integer, Integer>map= new HashMap<>();
+            for(int i=0; i<ans.length; i++){
+                map.put(ans[i], map.getOrDefault(ans[i], 0)+1);
+                if(map.get(ans[i])> ans.length/2 ){
+                    return ans[i];
+                }
+            }
+        
+        return  -1;
+    }
+   }
+    
+}
