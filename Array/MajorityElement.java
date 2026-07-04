dde02f1bad63c4ede4d7019236e3519239d4f6f3
Sahilsanjaysingh <7814384979sahil@gmail.com>
2026-07-04 21:37:43 +0530

    MajorityElement Array sort

--- Array/MajorityElement.java
+++ Array/MajorityElement.java
@@ -14,14 +14,25 @@ public static void main(String[] args) {
    }
    static class  Solution{
     public int Majority(int ans[]){
-        HashMap<Integer, Integer>map= new HashMap<>();
-            for(int i=0; i<ans.length; i++){
-                map.put(ans[i], map.getOrDefault(ans[i], 0)+1);
-                if(map.get(ans[i])> ans.length/2 ){
-                    return ans[i];
-                }
+        // HashMap<Integer, Integer>map= new HashMap<>();
+        //     for(int i=0; i<ans.length; i++){
+        //         map.put(ans[i], map.getOrDefault(ans[i], 0)+1);
+        //         if(map.get(ans[i])> ans.length/2 ){
+        //             return ans[i];
+        //         }
+        //     }
+
+        int len =ans.length/2;
+        int count =0;
+        int element=ans[0];
+        Arrays.sort(ans);
+        for(int i=0; i<ans.length; i++){
+            ans[i]= element;
+            count++;
+            if(count >=len){
+                return ans[i];
             }
-        
+        }
         return  -1;
     }
    }
